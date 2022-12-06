2a03bd9a5c1816018a6b11785807c09ccdff2d58
sunghogo <70732249+sunghogo@users.noreply.github.com>
2022-12-05 19:36:16 -0500

    Created basic flask webserver

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CSE442Team20FinancialWebApp
+Type 'flask run' from the 'CSE442Team20FinancialWebApp' folder to start the flask web server
---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    Flask.run(app, "0.0.0.0", 5000, True)
