424a247955f2d8af04c8f004315bc64cb091fd37
sunghogo <70732249+sunghogo@users.noreply.github.com>
2022-12-11 13:55:37 -0500

    linked up routing to pages

--- app.py
+++ app.py
@@ -7,5 +7,33 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
+@app.route('/coursehelp')
+def coursehelp():
+    return render_template('coursehelp.html')
+
+@app.route('/forum')
+def forum():
+    return render_template('forum.html')
+
+@app.route('/thread')
+def thread():
+    return render_template('thread.html')
+
+@app.route('/calculator')
+def calculator():
+    return render_template('calculator.html')
+
+@app.route('/calendar')
+def calendar():
+    return render_template('calendar.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/testing')
+def testing():
+    return render_template('testing.html')
+
 if __name__ == "__main__":
     Flask.run(app, "0.0.0.0", 5000, True)
